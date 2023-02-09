64ddca5ffb42627ebe9907aa1ec16e04cfbf6bb6
haller33 <eliseu42lucena@gmail.com>
2023-02-08 23:36:49 -0300

    new style stuff

--- build-site.el
+++ build-site.el
@@ -24,7 +24,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      org-html-head "<link rel=\"stylesheet\" href=\"https://haller33.github.com/css/simple.min.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
